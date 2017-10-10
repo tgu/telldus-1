0be799fb198b1a03340dd75a91f74e5d6bff7bae
Jørgen Blakstad <Jorgen.Blakstad@datametrix.no>
2017-10-10 22:10:17 +0200

    Updated to support sensors

--- examples/python/live/tdtool/tdtool.py
+++ examples/python/live/tdtool/tdtool.py
@@ -3,7 +3,14 @@
 
 import sys, getopt, httplib, urllib, json, os
 import oauth.oauth as oauth
+import datetime
 from configobj import ConfigObj
+
+import sys, codecs
+# set up output encoding
+if not sys.stdout.isatty():
+        # here you can set encoding for your 'out.txt' file
+        sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 PUBLIC_KEY = ''
 PRIVATE_KEY = ''
@@ -64,7 +71,32 @@ def printUsage():
 	print("             be an integer of the device-id")
 	print("             Device-id and name is outputed with the --list option")
 	print("")
+	print("       --list-sensors (-s short option)")
+	print("             Lists currently configured sensors")
+	print("")
+	print("       --sensor-data sensor (-d short option)")
+	print("             Get sensor data with sensor id number")
+	print("")
 	print("Report bugs to <info.tech@telldus.se>")
+
+def listSensors():
+        response = doRequest('sensors/list', {'includeIgnored': 1,'includeValues': 1,'includeScale': 1});
+        print("Number of sensors: %i" % len(response['sensor']));
+        for sensor in response['sensor']:
+                lastupdate = datetime.datetime.fromtimestamp(int(sensor['lastUpdated']));
+                for data in sensor['data']:
+                        if (data['name'] == "temp"):
+                                temperature = data['value'];
+                        elif (data['name'] == "humidity"):
+                                humidity = data['value'];
+                print "%s\t%s\t%s\t%s\t%s\t%s" % (sensor['id'], sensor['name'], lastupdate, sensor['battery'], temperature, humidity)
+
+def getSensorData(sensorId):
+	response = doRequest('sensor/info', {'id': sensorId });
+	lastupdate = datetime.datetime.fromtimestamp(int(response['lastUpdated']));
+	sensor_name = response['name'];
+	for data in response['data']:
+		print "%s\t%s\t%s\t%s" % (sensor_name, data['name'], data['value'], lastupdate)
 
 def listDevices():
 	response = doRequest('devices/list', {'supportedMethods': SUPPORTED_METHODS})
@@ -196,7 +228,7 @@ def main(argv):
 		authenticate()
 		return
 	try:
-		opts, args = getopt.getopt(argv, "ln:f:d:b:v:h", ["list", "on=", "off=", "dim=", "bell=", "dimlevel=", "up=", "down=", "help"])
+		opts, args = getopt.getopt(argv, "lsd:n:f:d:b:v:h", ["list", "list-sensors", "sensor-data=", "on=", "off=", "dim=", "bell=", "dimlevel=", "up=", "down=", "help"])
 	except getopt.GetoptError:
 		printUsage()
 		sys.exit(2)
@@ -209,6 +241,12 @@ def main(argv):
 
 		elif opt in ("-l", "--list"):
 			listDevices()
+
+		elif opt in ("-s", "--list-sensors"):
+			listSensors()
+
+		elif opt in ("-d", "--sensor-data"):
+			getSensorData(arg)
 
 		elif opt in ("-n", "--on"):
 			doMethod(arg, TELLSTICK_TURNON)
@@ -235,5 +273,5 @@ def main(argv):
 			doMethod(arg, TELLSTICK_DOWN)
 
 if __name__ == "__main__":
-	config = ConfigObj(os.environ['HOME'] + '/.config/Telldus/tdtool.conf')
+	config = ConfigObj('/.config/Telldus/tdtool.conf')
 	main(sys.argv[1:])
